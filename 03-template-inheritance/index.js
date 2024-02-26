fa07575dfc825a03f56c4ca200834e66aab6c286
kunxin-chor <chorkunxin@yahoo.com>
2024-02-26 06:52:02 +0000

    Added template inheritance example

--- 03-template-inheritance/index.js
+++ 03-template-inheritance/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const hbs = require('hbs');
+const waxOn = require('wax-on');
+
+// create a new Express application
 const app = express();
 
 // inform Express that we are using hbs as the view engine
 app.set('view engine', 'hbs');
 
+waxOn.on(hbs.handlebars);  // apply wax-on to handlebars
+waxOn.setLayoutPath('./views/layouts'); // where to find the layout
+
 // setup routes
 app.get('/', function(req,res){
-    res.send("hello world");
+    res.render('index');
+})
+
+app.get('/about-us', function(req,res){
+    res.render('about');
+})
+
+app.get('/contact-us', function(req,res){
+    res.render('contact');
 })
 
 // start server
