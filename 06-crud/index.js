bdc50a952f216242c64d2ab6c077f50f4cb12a95
kunxin-chor <chorkunxin@yahoo.com>
2024-03-04 05:38:20 +0000

    Added U

--- 06-crud/index.js
+++ 06-crud/index.js
@@ -1,10 +1,19 @@
 // 1. SETUP
 const express = require('express'); 
 const hbs = require('hbs');
+
+// setup the 188 handlebars helpers
+const handlebarHelpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+});
+
+
 const wax = require('wax-on');
 
 const app = express(); // create an express application
 app.set('view engine', 'hbs'); // set express to use hbs as our view engine
+
+
 
 // setup wax on
 wax.on(hbs.handlebars);
@@ -15,7 +24,7 @@ app.use(express.urlencoded({
     extended: false
 }))
 
-// In-memory database
+// In-memory database (global array)
 const foodRecords = [
     {
         id: 1,
@@ -29,7 +38,7 @@ const foodRecords = [
         foodName:"Boston Clam Chowder",
         calories: 750,
         meal:"dinner",
-        tags:["home-cooked"]
+        tags:["homecooked"]
     },
     {
         id: 3,
@@ -81,6 +90,84 @@ app.post('/create', function(req,res){
     // to a different url on the same server (if the url is relative)
     res.redirect("/");
     console.log(req.body);
+})
+
+// the following route has a parameter, named 'foodid'
+
+app.get('/delete-food/:foodid', function(req,res){
+    const idToDelete = req.params.foodid;
+
+    // linear search algo
+    let wantedFoodRecord = null;
+    for (let record of foodRecords) {
+        if (record.id == idToDelete) {
+            wantedFoodRecord = record;
+            break;
+        }
+    }
+
+    res.render('confirm-delete', {
+        "record": wantedFoodRecord
+    })
+})
+
+app.post('/delete-food/:foodid', function(req,res){
+    // we need to know the index of the element
+    // that we want to delete
+    const idToDelete = req.params.foodid;
+
+    // find the index of the food record
+    let indexToDelete = -1;
+    for (let i = 0; i < foodRecords.length; i++) {
+        if (foodRecords[i].id == idToDelete) {
+            indexToDelete = i;
+            break;
+        }
+    }
+
+    // use splice to delete an index from an array
+    // splice has 2 parameters:
+    // parameter 1: the index to start deleting from
+    // parameter 2: how many to delete 
+    foodRecords.splice(indexToDelete, 1);
+    res.redirect('/');
+})
+
+app.get('/edit-food/:foodid', function(req,res){
+    const idToEdit = req.params.foodid;
+    // get the food record that we are editing
+    const foodRecordToEdit = foodRecords.find(function(record){
+        return record.id == idToEdit;
+    })
+    res.render('edit', {
+        record: foodRecordToEdit
+    })
+
+})
+
+app.post('/edit-food/:foodid', function(req,res){
+    const idToEdit = req.params.foodid;
+    const indexToEdit = foodRecords.findIndex(function(record){
+        return record.id == idToEdit;
+    });
+
+    let tags = [];
+    if (Array.isArray(req.body.tags)){
+        tags = req.body.tags;
+    } else if (req.body.tags) {
+        tags = [ req.body.tags ];
+    }
+
+    const modifiedRecord = {
+        "id": req.params.foodid,
+        "foodName": req.body.foodName,
+        "calories": req.body.calories,
+        "meal": req.body.meal,
+        "tags": tags
+
+    }
+    foodRecords[indexToEdit] = modifiedRecord;
+    res.redirect('/');
 })
 
 // 3. START SERVER
