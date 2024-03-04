8cc5031ec42d755cf795344a468cb5fc2ec8c080
kunxin-chor <chorkunxin@yahoo.com>
2024-03-04 07:07:50 +0000

    Added delete for crud hands on

--- 06b-crud-hands-on/index.js
+++ 06b-crud-hands-on/index.js
@@ -89,6 +89,57 @@ app.post("/create-listing", function(req,res){
 
 })
 
+// A route to display a form to confirm the deletion
+// We will use a route parameter to specify which ID to delete
+app.get('/delete-listing/:listingid', function(req,res){
+    // 1. get the ID that the user wants to delete
+    const idToDelete = req.params.listingid;
+
+    // 2. retrieve the listing object based on its ID
+    // let listingToDelete = database.find(function(record){
+    //     return record.id == idToDelete;
+    // })
+    let listingToDelete = null;
+    for (let record of database) {
+        if (record.id == idToDelete) {
+            listingToDelete = record;
+            break;
+        }
+    }
+
+    // 3. show a form to ask the user if they are really
+    // sure they want to delete
+    res.render('confirm-delete', {
+        "record": listingToDelete
+    }) 
+})
+
+// route to confirm the submission of the form
+app.post('/delete-listing/:listingid', function(req,res){
+    // 1. get the ID of the listing that we want to delete
+    const idToDelete = req.params.listingid;
+
+    // 2. get the INDEX of the listing that we want to delete
+    // (to delete an item from an array using the splice function,
+    // we need the index)
+
+    // const indexToDelete = database.findIndex(function(record){
+    //     return record.id == idToDelete;
+    // })
+
+    let indexToDelete = -1;
+    for (let i = 0; i < database.length; i++) {
+        if (database[i].id == idToDelete) {
+            indexToDelete = i;
+            break;
+        }
+    }
+
+    // 3. remove the item from the array
+    database.splice(indexToDelete, 1);
+
+    res.redirect('/');
+})
 
 // 3. START SERVER
 app.listen(3000, function(){
